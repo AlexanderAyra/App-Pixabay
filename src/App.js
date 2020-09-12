0277dcf7bbd73deb2a2a1f34630c854b53b3989f
Alexander <alexander.ayras@gmail.com>
2020-09-11 23:31:51 -0500

    Mostrando las imagenes y enlace para alta resolucion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Formulario from './components/Formulario'
+import ListaImagenes from './components/ListaImagenes'
 
 const App = () => {
 
   const [busqueda, setBusqueda] = useState('')
+  const [imagenes, setImagenes] = useState([])
 
   useEffect(() => {
     
@@ -11,13 +13,13 @@ const App = () => {
 
       if(busqueda === '') return
 
-      const imagesPorPagina = 30
+      const imagesPorPagina = 20
       const key = '18275283-6af94f0e1d9358cfaed0f745c'
       const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagesPorPagina}`
       const respuesta = await fetch(url)
       const api = await respuesta.json()
 
-      setBusqueda(api.hits)
+      setImagenes(api.hits)
 
     }
     sonsultarAPI()
@@ -33,6 +35,11 @@ const App = () => {
         <Formulario
           setBusqueda={setBusqueda}
         />
+      </div>
+      <div className="row justify-content-center">
+          <ListaImagenes
+            imagenes={imagenes}
+          />
       </div>
     </div>
   );
